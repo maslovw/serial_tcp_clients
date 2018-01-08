96fb84e866459e86d9ce09462ee161af207cfefe
Viacheslav Maslov <viacheslav.maslov@esrlabs.com>
2018-01-08 08:40:20 +0100

    add tcp_server.py so no need to change old startup scripts

--- serialtcp/__init__.py
+++ serialtcp/__init__.py
@@ -1,8 +1,7 @@
 import logging
 logging.getLogger(__name__).addHandler(logging.NullHandler())
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.WARNING)
 import sys
 import os
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
-print("init")
