fe608a8902e94a04a00275ffc1995a4f0a128e57
Viacheslav Maslov <viacheslav.maslov@esrlabs.com>
2018-01-08 16:40:11 +0100

    Add MIT License
    Update meta data

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='serial_tcp_clients',
-    version='2.00',
+    version='2.0.1-dev',
     packages=['serialtcp'],
     install_requires=['pyserial>=3.3'],
-    url='',
-    license='',
+    python_requires='~=3.5',
+    url='https://github.com/maslovw/serial_tcp_clients',
     author='maslovw',
     author_email='serialtcp@maslovw.com',
+    license='MIT',
+    keywords='serial com port tcp server socket',
     description='share serial device through tcp connections'
 )
